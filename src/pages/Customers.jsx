a1a546b6c45d1f4179d4a519336e9e903a2c5fac
Rijan Tiwari <reezantiwaree@gmail.com>
2025-10-18 13:55:29 +0545

    Customers page added with the delete functionality

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Page,
+  Selection,
+  Inject,
+  Edit,
+  Toolbar,
+  Sort,
+  Filter,
+} from "@syncfusion/ej2-react-grids";
+import { customersData, customersGrid } from "../data/dummy";
+import { Header } from "../components";
 
 const Customers = () => {
-  return <div>Customers</div>;
+  return (
+    <div className="m-2 md:m-10 p-2 md:10">
+      <Header category="Page" title="Customers" />
+      <GridComponent
+        dataSource={customersData}
+        allowExcelExport
+        allowSorting
+        allowPaging
+        toolbar={["Delete"]}
+        width="auto"
+        editSettings={{ allowDeleting: true, allowEditing: true }}
+      >
+        <ColumnsDirective>
+          {customersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Page, Selection, Edit, Sort, Filter, Toolbar]} />
+      </GridComponent>
+    </div>
+  );
 };
 
 export default Customers;
